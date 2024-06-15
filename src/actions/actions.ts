541a92fad90ecf2b58559cf783e2ca0c13447403
Solo Dev is Good <work.abdelrahman@outlook.com>
2024-06-15 22:19:20 +0200

    Update actions.ts

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -3,8 +3,7 @@ import { DataResult } from "@/lib/types";
 import { Project } from "@/lib/types";
 
 export const getProjects = async (): Promise<DataResult> => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/projects.json`);
-  if (!response.ok) {
+  const response = await fetch(`https://solofox.vercel.app/projects.json`)
     return {
       data: null,
       isError: true,
@@ -21,7 +20,7 @@ export const getProjects = async (): Promise<DataResult> => {
 }
 
 export const getProjectById = async (id: number): Promise<DataResult> => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/projects.json`);
+  const response = await fetch(`https://solofox.vercel.app/projects.json`);
   if (!response.ok) {
     return {
       data: null,
